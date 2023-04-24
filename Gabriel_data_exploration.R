39d6f4587b1aae36d56409b2ae5b0f8b8e6338cc
Gabriel Battcock <gabrielbattcock@gmail.com>
2023-04-24 14:16:52 +0100

    exploring food index

--- Gabriel_data_exploration.R
+++ Gabriel_data_exploration.R
@@ -24,6 +24,7 @@ nrow(user$SUBJECT)
 names(user)
 hist(user$WEIGHT)
 hist(user$HEIGHT)
+hist(user$AGE_YEAR)
 unique(user$ADM1_NAME) #not all states included
 unique(user$ADM2_NAME)
 
@@ -57,13 +58,28 @@ which.max(total_vitA$sum)
 
 
 MICRONUT_SUM <- function(data, micronutrient){
-  total <- data %>% 
+  # takes in the data frame and micronutrient wanted and calculates the sum for each subject
+  data %>% 
     group_by(SUBJECT, ROUND.x) %>% 
-    summarise("sum_{{micronutrient}}" := with(data,sum({{micronutrient}}))) %>% 
-    select(SUBJECT, ROUND.x, "sum_{{micronutrient}}")
-  total
+    summarise("sum_{{micronutrient}}" := sum({{micronutrient}})) #%>% 
+    # select(SUBJECT, ROUND.x, sum)
+  # total
 }
 
 vit_a <- MICRONUT_SUM(joined, VITA_RAE_mcg)
 vit_c <- MICRONUT_SUM(joined, VITC_mg)
 thia <- MICRONUT_SUM(joined, THIA_mg)
+
+hist(vit_c$sum)
+hist(thia$sum)
+### this all works, this can help define the target 
+
+grep("A03V",joined$FOODEX2_INGR_CODE)#A03V is where composite foods begin in the classification scheme -- makes grouping foods more difficult
+grep("A03X",joined$FOODEX2_INGR_CODE)
+grep("A03Y",joined$FOODEX2_INGR_CODE)
+joined$FOODEX2_INGR_CODE[297216]
+
+#### how to group codes together to create a covariate
+#### what we want is a data frame with one row for each subject, a target variable (yes/no inadequate intake of MN)
+#### and a yes/no covarate for different food groups. 
+#### Need to search the food ingredient code column for specific indicies and mutate to a new column with 1 or 0
