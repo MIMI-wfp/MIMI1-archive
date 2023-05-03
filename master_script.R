87513ae4840ce8d5af7c5d9778656be839352f39
Gabriel Battcock <gabrielbattcock@gmail.com>
2023-05-03 16:04:47 +0100

    creating a master script

---
+++ master_script.R
@@ -0,0 +1,9 @@
+### Gabriel Battcock
+### Master file 
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))#sets wd to where this is saved, with the other running scripts
+
+#create the data frames
+source("data_loading.R")
+
+# run any ML scripts
