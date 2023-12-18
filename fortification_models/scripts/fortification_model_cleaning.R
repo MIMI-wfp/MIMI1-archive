e7c7bfb6cd18e48e1ba8c2fecd3783873af32f02
osman-mo94 <osman.mo94@gmail.com>
2023-12-18 11:54:25 +0000

    base model functions
    Minor edits and comments to base model functions script.

--- fortification_models/scripts/fortification_model_cleaning.R
+++ fortification_models/scripts/fortification_model_cleaning.R
@@ -6,8 +6,27 @@
 # Date created: 18-Dec-2023
 # Last edited: 
 
+# Install and load required packages:
+rq_packages <- c("readr", "tidyverse")
+
+installed_packages <- rq_packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(rq_packages[!installed_packages])
+}
+
+lapply(rq_packages, require, character.only = T)
+
+rm(list= c("rq_packages", "installed_packages"))
+
+#-------------------------------------------------------------------------------
+
+# Source script required to create base models: 
+source("all_base_models/scripts/base_model_functions.R")
+
 #-------------------------------------------------------------------------------
 
 # NIGERIA
 
 # Read-in base model:
+nga_food_consumption <- read_csv("data/nga1819_food_consumption.csv")
+nga
