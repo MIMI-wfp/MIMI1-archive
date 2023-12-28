cba8fab4552a725cd534280c2c336f9c40f84737
osman-mo94 <osman.mo94@gmail.com>
2023-12-28 13:42:46 +0000

    Create function to create a data-frame that indicates access to each fortification vehicle, and quantity consumed (for each household).
    Clean data for Nigeria and Malawi and apply function.

---
+++ fortification_models/scripts/fortification_model_functions.R
@@ -0,0 +1,105 @@
+################################################################################
+####################### FORTIFICATION MODEL FUNCTIONS ##########################
+################################################################################
+
+# Author: Mo Osman
+# Collaborators: Gabriel Battcock & Kevin Tang
+# Date created: 28-Dec-2023
+# Last edited: 28-Dec-2023
+
+# This script contains functions required for creating the fortification models.
+
+# INSTALL AND LOAD PACKAGES: 
+
+rq_packages <- c("tidyverse")
+
+installed_packages <- rq_packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(rq_packages[!installed_packages])
+}
+
+lapply(rq_packages, require, character.only = T)
+
+rm(list= c("rq_packages", "installed_packages"))
+
+#-------------------------------------------------------------------------------
+
+# Function to get access and quantities of each fortrification vehicle: 
+
+get_vehicle_quantities <- function(base_ai, food_consumption) {
+  vehicle_quantities <<- base_ai %>% select("hhid") %>% 
+    # RICE
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Rice",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("rice_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(rice = "Yes") %>% 
+                 dplyr::select("hhid", "rice", "rice_100g")), by = "hhid") %>% 
+    # WHEAT FLOUR:
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Wheat flour",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("wheatflour_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(wheatflour = "Yes") %>% 
+                 dplyr::select("hhid", "wheatflour", "wheatflour_100g")), by = "hhid") %>% 
+    # MAIZE FLOUR:
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Maize flour",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("maizeflour_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(maizeflour = "Yes") %>% 
+                 dplyr::select("hhid", "maizeflour", "maizeflour_100g")), by = "hhid") %>%
+    # SUGAR:
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Sugar",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("sugar_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(sugar = "Yes") %>% 
+                 dplyr::select("hhid", "sugar", "sugar_100g")), by = "hhid") %>%
+    # EDIBLE OIL:
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Edible oil",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("edible_oil_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(edible_oil = "Yes") %>% 
+                 dplyr::select("hhid", "edible_oil", "edible_oil_100g")), by = "hhid") %>%
+    # SALT:
+    left_join((food_consumption %>%
+                 dplyr::select("hhid", "food_item", "consumed", "food_purchased",
+                               "quantity_100g") %>%
+                 filter(food_item == "Salt",
+                        consumed == "Yes",
+                        food_purchased == "Yes") %>%
+                 rename("salt_100g" = "quantity_100g") %>%
+                 ungroup() %>% 
+                 mutate(salt = "Yes") %>% 
+                 dplyr::select("hhid", "salt", "salt_100g")), by = "hhid") %>%
+    # If food item is not consumed, then replace NA with "No":
+    mutate(across(c(rice, wheatflour, maizeflour, sugar, edible_oil, salt), 
+                  ~ ifelse(is.na(.), "No", .)))%>% 
+    # Change food item variables to factor: 
+    mutate(across(c(rice, wheatflour, maizeflour, sugar, edible_oil, salt), 
+                  ~ factor(., levels = c("Yes", "No"))))
+}
+
+#-------------------------------------------------------------------------------
